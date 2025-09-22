21bb260f33fac29885c2e345e11046be67af03a1
Yuri Sampaio <sampaio.yuri31@gmail.com>
2025-09-21 22:14:36 -0300

    criando a pagina de login

--- consumo_IA/usuarios/urls.py
+++ consumo_IA/usuarios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path #função que cria urls
 from . import views #import o arquivo viewa 
 
 urlpatterns = [
-    path('cadastro/' , views.cadastro , name='cadastro')
+    path('cadastro/' , views.cadastro , name='cadastro'),
+    path('login/' , views.login , name='login'),
 ]
