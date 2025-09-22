16e2fd20780166df7e5337eddd5a7741b4dbc358
Yuri Sampaio <sampaio.yuri31@gmail.com>
2025-09-21 21:46:01 -0300

    definindo as mensagens de erro para o usuario

--- consumo_IA/usuarios/views.py
+++ consumo_IA/usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect #render= renderiza as paginas html; redirect= redireciona
 from django.http import HttpResponse #função para retornar uma resposta http pro usuario
 from django.contrib.auth.models import User #tabela do banco de dados 
+from django.contrib.messages import constants #tipo de erro
+from django.contrib import messages #função que cria a msg
 
 # Create your views here.
 
@@ -17,17 +19,19 @@ def cadastro(request):
 
         #verifica se as senhas são iguais
         if senha != confirmar_senha:
-            print("ERRO 2")
+            messages.add_message(request, constants.ERROR, "As senhas devem ser iguais")
             return redirect('cadastro')
         
         #verifica se a senha tem menos de 6 digitos
         if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, "As senhas devem ter mais de 6 digitos")
             return redirect('cadastro')
         
         #verifica no db se tem usuarios com o username escolhido
         user = User.objects.filter(username=username)
 
         if user.exists():
+            messages.add_message(request, constants.ERROR, "Já existe um usuário com esse username")
             return redirect('cadastro')
 
         #armazena o que foi digitado em uma tabela no db
@@ -38,4 +42,9 @@ def cadastro(request):
         )
            
 
-        return HttpResponse (f'{username}')
+        return redirect('login')
+    
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
