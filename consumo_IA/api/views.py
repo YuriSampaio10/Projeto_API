ee87a318f5783667ef185598f3deb741c6503622
Yuri Sampaio <sampaio.yuri31@gmail.com>
2025-09-28 18:41:36 -0300

    iniciando a pagina html do chatbot e criando as funcoes

--- consumo_IA/api/views.py
+++ consumo_IA/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import openai
+from django.http import HttpResponse
 
 # Create your views here.
+
+def ia(request):
+    if request.method == "GET":
+        return render(request, 'ia.html')
+    elif request.method == "POST":
+        question = request.POST.get('question')
+
+        # client = openai.OpenAI(api_key='##############')
+        result = client.chat.completions.create(
+            model='gpt-4o-mini',
+            messages=[
+                {'role': 'system', 'content': "Você é um assistente pessoal, tire todas as duvidas."},
+                {'role': 'user', 'content': question}
+            ]
+        )
+        result HttpResponse(result)
