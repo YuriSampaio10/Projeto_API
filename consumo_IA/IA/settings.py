0ac3c9e9fd73dbe6097177feae6af93f96407a36
Yuri Sampaio <sampaio.yuri31@gmail.com>
2025-09-28 20:40:02 -0300

    resposta da ia OK

--- consumo_IA/IA/settings.py
+++ consumo_IA/IA/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 #bibliotecas
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- consumo_IA/api/views.py
+++ consumo_IA/api/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect #render= renderiza as paginas html; redirect= redireciona
+from django.http import HttpResponse, StreamingHttpResponse #função para retornar uma resposta http pro usuario
+from django.contrib.auth.models import User #tabela do banco de dados 
+from django.contrib.messages import constants #tipo de erro
+from django.contrib import messages #função que cria a msg
 import openai
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -10,12 +13,23 @@ def ia(request):
     elif request.method == "POST":
         question = request.POST.get('question')
 
-        # client = openai.OpenAI(api_key='##############')
-        result = client.chat.completions.create(
+        client = openai.OpenAI(api_key='#################################################')
+      
+        def stream_gpt():
+            result = client.chat.completions.create(
             model='gpt-4o-mini',
             messages=[
-                {'role': 'system', 'content': "Você é um assistente pessoal, tire todas as duvidas."},
+                {'role': 'system', 'content': f"Você é um assistente pessoal, tire todas as duvidas."},
                 {'role': 'user', 'content': question}
-            ]
-        )
-        result HttpResponse(result)
+                ],
+                stream=True
+            )
+
+            for chunk in result: 
+                yield chunk.choices[0].delta.content
+
+        response_server = StreamingHttpResponse(stream_gpt())
+        response_server['Cache-Control'] = 'no-cache'
+        response_server['X-acceel-Buffering'] = 'no'
+
+        return response_server
